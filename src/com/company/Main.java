b007e105017bfbf2287bef2dc94dd28caa7aa3f6
bensonc <craig.benson@ppb.com>
2019-09-04 17:06:17 +0100

    created jumbled text, working on inserting and replacing wordsbut might skip and enter them and begin query forwards, back, up down and diag.

---
+++ src/com/company/Main.java
@@ -0,0 +1,78 @@
+package com.company;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader("C:\\Users\\bensonc\\Documents\\Projects\\Crossword\\Test.txt"));
+        Random rndPosition = new Random();
+
+        String text = "hello";
+        int row = 10;
+        int column = 10;
+        //variable generates random number for index of where to put the word int the line
+        int position = rndPosition.nextInt(column);
+        int intTextLength = text.length();
+        String replaced;
+        int acc = 0;
+
+        //calls and passes how many rows and columns,
+        createTable(row, column);
+
+
+        //reads and stores the first line
+        String Line = bufferedReader.readLine();
+
+        //returns the position 'hello' will be placed
+        position = checkLength(position, column, intTextLength, acc);
+
+        //get the string to be replaced
+        char firstChar = Line.charAt(position);
+        char endChar = Line.charAt(text.length() - 1);
+        replaced = Line.substring(position);
+
+        //you stopped here
+        Line.replace(replaced, text);
+
+    }
+    //check length to see if hello is in range forward
+    public static int checkLength(int position, int length, int textLength, int Acc) {
+
+        Random rndPos = new Random();
+
+        if (position == 0) {
+            return Acc;
+        }
+        if ((length - position) >= textLength) {
+            return Acc = position;
+
+        }
+        return (checkLength(position = rndPos.nextInt(length), length, textLength, Acc));
+
+
+    }
+
+
+    //change to Recursion, creates the a textfile and writes random charachters
+    public static void createTable(int numRows, int numColumn) throws IOException {
+        PrintWriter printWriter = new PrintWriter("test.txt", StandardCharsets.UTF_8);
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("C:\\Users\\bensonc\\Documents\\Projects\\Crossword\\Test.txt", true));
+        Random rnd = new Random();
+
+        for (int j = 0; j < numRows; j++) {
+
+            for (int i = 0; i < numColumn; i++) {
+                char c = (char) (rnd.nextInt(26) + 'a');
+                bufferedWriter.append(c);
+            }
+            bufferedWriter.newLine();
+
+        }
+        bufferedWriter.close();
+
+
+    }
+}
